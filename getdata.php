ec4df21636de22d559dc8dd33e9df1d0c58406d7
landoncuff <landoncuff23@gmail.com>
2022-03-07 16:16:32 -0700

    testing

--- getdata.php
+++ getdata.php
@@ -1,13 +1,30 @@
 <?php
-$file = fopen("randySampleData1.csv", "r");
+$file = fopen("randySampleData1.csv","r");
 
-$rows = fgetcsv($file, 30, ",");
+//$rows = fgetcsv($file, 30, ",");
 
-// $i = 0;
+// while(!feof($file)){
+//     $content = fgetcsv($file);
 
-// while($rows = fgetcsv($file, 30, ",")){
-//     $i++;
-//     echo $rows[2];
+//     $count = count($content);
 
-// };
+//     for($i=0; $i<$count;$i++){
+//         echo $content[1]."\t";
+//     }
+// echo "<br/>";
+// }
+
+$i = 0;
+while(!feof($file)){
+    while($rows = fgetcsv($file, ",")){
+        $i++;
+        echo $rows[2]."\t";
+    
+        if($i == 10){
+            break;
+        }
+    
+    }
+}
+
 ?>
--- index.php
+++ index.php
@@ -9,32 +9,86 @@ require 'getdata.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Document</title>
+    <style>
+        table {
+          font-family: arial, sans-serif;
+          border-collapse: collapse;
+          width: 80%;
+          margin-left: 10%;
+          margin-top: 50px;
+        }
+        
+        td, th {
+          border: 1px solid #dddddd;
+          text-align: left;
+          padding: 8px;
+        }
+        
+        tr:nth-child(even) {
+          /* background-color: #dddddd; */
+        }
+
+        .head{
+            width: 50%;
+            height: 20%;
+            text-align: center;
+            color: white;
+            background-color: grey;
+        }
+        .infoTable{
+            margin-left: 20%;
+        }
+        </style>
 </head>
 <body>
-    <h1>Landon</h1>
-    <table>
-        <thead>
+    <h1>Students Name</h1>
+    <table class="infoTable">
             <tr>
-                <th>First Name</th>
-                <th>Last Name</th>
+                <th class="head" colspan="3">CONTACT INFORMATION</th>
             </tr>
-        </thead>
-    <tbody>
-        <?php 
-        $i = 0;
-        while($rows){$i++
-        ?>
+            <tr>
+                <td>Athlete Name</td>
+                
+                <td>Cell Phone</td>
+                <td>Email</td>
+            </tr>
+            <tr>
+                <td>Parent 1</td>
+                <td>Parent 1 Phone</td>
+                <td>Parent 1 email</td>
+            </tr>
+            <tr>
+                <td colspan="3">Address Information</td>
+            </tr>
+    </table>
+    <table>
         <tr>
-            <td><?php echo $i; ?></td>
-            <td><?php echo $rows[3]; ?></td>
-            <td><?php echo $rows[2]; ?></td>
-            <td><?php echo $rows[5]; ?></td>
+            <th class="head" colspan="4">Personal Information</th>
         </tr>
-        <?php 
-        }
-        ?>
-    </tbody>
-    </table>  
+        <tr>
+            <td colspan="4">High School Name</td>
+        </tr>
+        <tr>
+            <td>Head Coach Name</td>
+            <td>Head Coach Phone</td>
+            <td colspan="2">Head Coach Email</td>
+        </tr>
+        <tr>
+            <td>Ethnicity</td>
+            <td>Hobbies</td>
+            <td>College Preference</td>
+            <td>Previous Injuries</td>
+        </tr>
+        <tr>
+            <td>Facebook</td>
+            <td>Twitter</td>
+            <td>Instagram Preference</td>
+            <td>Snapchat</td>
+        </tr>
+       
+</table>
+   
 </body>
 </html>
